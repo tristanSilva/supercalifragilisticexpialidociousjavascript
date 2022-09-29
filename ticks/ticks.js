8879476afd9e8d55397647587dde61595b1bc0b6
TristanSilva <tristansilva@gmail.com>
2022-09-29 08:11:45 +0800

    commit changes
    commit changes

--- ticks/ticks.js
+++ ticks/ticks.js
@@ -9,6 +9,10 @@ let age = prompt("What's your age? ")
 const response = "You're " + age + ", old."
 console.log(response)
 
-// use ticks
+// use ticks - string interpolation
 const ticksResponse = `You're ${age}, old.`
 console.log(ticksResponse)
+
+const heavy = 2500;
+const heavyResponse = `Heavy is ${heavy + 300} the weight.`
+console.log(heavyResponse)
