61fcaa64eaa7aa5d654f831fccacdc741f1fa464
TristanSilva <tristansilva@gmail.com>
2022-09-29 08:41:50 +0800

    commit changes
    commit changes

--- shorten/shorten.js
+++ shorten/shorten.js
@@ -2,5 +2,11 @@ const authorizedCenter = false;
 
 // shorten  - conditionals with ternaries
 const isisAuthorized = authorizedCenter ? true : false;
-
 console.log(isisAuthorized)
+
+// multiple conditions - 
+// instead of doing this use the if...else if ...  else statement
+// that will be easy to read and understand
+const givemeaNumber = 20;
+const numberResponse = givemeaNumber < 10 ? "'<10'":  givemeaNumber > 18 ? "'18'" : givemeaNumber > 10 ? "'>10'":"older";
+console.log(numberResponse)
